606094e59ded5bb0e095db6756d3f0f43420ae99
Illia Smirnov <smirnov.illia@gmail.com>
2018-09-29 14:24:41 +0300

    no message

--- dao-jdbc-impl/src/main/java/com/itacademy/jd2/is/carsharing/dao/jdbc/impl/entity/CarServiceDetail.java
+++ dao-jdbc-impl/src/main/java/com/itacademy/jd2/is/carsharing/dao/jdbc/impl/entity/CarServiceDetail.java
@@ -4,8 +4,8 @@ import com.itacademy.jd2.is.carsharing.dao.api.entity.IServiceOperation;
 
 public class CarServiceDetail extends BaseEntity{
 	
-	private IService service;
-	private IServiceOperation serviceOperation;
+//	private IService service;
+//	private IServiceOperation serviceOperation;
 	
 	
 
