f4f573c705cfed6665c4c17cac40fa42a7e43c6a
smirnov <smirnov.illia@gmail.com>
2018-10-09 00:00:07 +0300

    working on tests

--- service/src/test/java/com/itacademy/jd2/is/carsharing/service/impl/AbstractTest.java
+++ service/src/test/java/com/itacademy/jd2/is/carsharing/service/impl/AbstractTest.java
@@ -4,24 +4,60 @@ import java.util.Random;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import com.itacademy.jd2.is.carsharing.dao.api.entity.IBlackList;
 import com.itacademy.jd2.is.carsharing.dao.api.entity.IBrand;
+import com.itacademy.jd2.is.carsharing.service.IBlackListService;
 import com.itacademy.jd2.is.carsharing.service.IBrandService;
+import com.itacademy.jd2.is.carsharing.service.ICarService;
+import com.itacademy.jd2.is.carsharing.service.ICarServiceDetailService;
+import com.itacademy.jd2.is.carsharing.service.ICarServiceHistoryService;
+import com.itacademy.jd2.is.carsharing.service.IColorService;
+import com.itacademy.jd2.is.carsharing.service.ICustomerService;
+import com.itacademy.jd2.is.carsharing.service.IInsuranceService;
+import com.itacademy.jd2.is.carsharing.service.IModelService;
+import com.itacademy.jd2.is.carsharing.service.IOrderHistoryService;
+import com.itacademy.jd2.is.carsharing.service.IServiceOperationService;
+import com.itacademy.jd2.is.carsharing.service.ISparePartService;
+import com.itacademy.jd2.is.carsharing.service.ITrackingService;
+import com.itacademy.jd2.is.carsharing.service.IUserAccountService;
 
-@SpringJUnitConfig(locations="classpath:service-context-test.xml")
+@SpringJUnitConfig(locations = "classpath:service-context-test.xml")
+@Sql
 public class AbstractTest {
 	@Autowired
 	protected IBrandService brandService;
+	@Autowired
+	protected IBlackListService blackListService;
+	@Autowired
+	protected ICarService carService;
+	@Autowired
+	protected ICarServiceDetailService carServiceDetailService;
+	@Autowired
+	protected ICarServiceHistoryService carServiceHistoryService;
+	@Autowired
+	protected IColorService colorService;
+	@Autowired
+	protected ICustomerService customerService;
+	@Autowired
+	protected IInsuranceService insuranceService;
+	@Autowired
+	protected IModelService modelService;
+	@Autowired
+	protected IOrderHistoryService orderHistoryService;
+	@Autowired
+	protected IServiceOperationService operationService;
+	@Autowired
+	protected ISparePartService sparePartService;
+	@Autowired
+	protected ITrackingService trackingService;
+	@Autowired
+	protected IUserAccountService userAccountService;
 
 	public AbstractTest() {
 		super();
-	}
-
-	@BeforeEach
-	public void setUpMethod() {
-		// clean DB recursive
-		brandService.deleteAll();
 	}
 
 	private static final Random RANDOM = new Random();
@@ -44,4 +80,10 @@ public class AbstractTest {
 		brandService.save(entity);
 		return entity;
 	}
+
+	protected IBlackList saveNewBlackList() {
+		final IBlackList entity = blackListService.createEntity();
+		return entity;
+	}
+
 }
--- service/src/test/java/com/itacademy/jd2/is/carsharing/service/impl/BrandServiceTest.java
+++ service/src/test/java/com/itacademy/jd2/is/carsharing/service/impl/BrandServiceTest.java
@@ -1,8 +1,12 @@
 package com.itacademy.jd2.is.carsharing.service.impl;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,10 +19,59 @@ public class BrandServiceTest extends AbstractTest {
 
 		final IBrand entityFromDb = brandService.get(entity.getId());
 
+		assertNotNull(entityFromDb);
 		assertEquals(entity.getName(), entityFromDb.getName());
 		assertNotNull(entityFromDb.getId());
 		assertNotNull(entityFromDb.getCreated());
 		assertNotNull(entityFromDb.getUpdated());
 		assertTrue(entityFromDb.getCreated().equals(entityFromDb.getUpdated()));
+	}
+
+	@Test
+	public void testUpdate() throws InterruptedException {
+		final IBrand entity = saveNewBrand();
+
+		String newName = entity.getName() + "_updated";
+		entity.setName(newName);
+		Thread.sleep(2000);
+		brandService.save(entity);
+		
+		final IBrand entityFromDb = brandService.get(entity.getId());
+
+		assertNotNull(entityFromDb);
+		assertEquals(newName, entityFromDb.getName());
+		assertNotNull(entityFromDb.getId());
+		assertNotNull(entityFromDb.getCreated());
+		assertNotNull(entityFromDb.getUpdated());
+		assertEquals(entity.getCreated(), entityFromDb.getCreated());
+		assertTrue(entityFromDb.getUpdated().after(entity.getCreated()));
+	}
+
+	@Test
+	public void testGetAll() {
+		final int intialCount = brandService.getAll().size();
+
+		final int randomObjectsCount = getRandomObjectsCount();
+		for (int i = 0; i < randomObjectsCount; i++) {
+			saveNewBrand();
+		}
+
+		final List<IBrand> allEntities = brandService.getAll();
+
+		for (final IBrand entityFromDb : allEntities) {
+			assertNotNull(entityFromDb.getName());
+			assertNotNull(entityFromDb.getId());
+			assertNotNull(entityFromDb.getCreated());
+			assertNotNull(entityFromDb.getUpdated());
+		}
+
+		assertEquals(randomObjectsCount + intialCount, allEntities.size());
+	}
+
+	@Test
+	public void testDelete() {
+		final IBrand entity = saveNewBrand();
+		brandService.delete(entity.getId());
+		assertNull(brandService.get(entity.getId()));
 	}
 }
