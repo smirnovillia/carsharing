f132873787e1c08324a5e016d148709846184e0a
Illia Smirnov <smirnov.illia@gmail.com>
2018-11-04 21:59:36 +0300

    no message

--- dao-orm-impl/src/main/java/com/itacademy/jd2/is/carsharing/dao/orm/impl/BrandDaoImpl.java
+++ dao-orm-impl/src/main/java/com/itacademy/jd2/is/carsharing/dao/orm/impl/BrandDaoImpl.java
@@ -2,33 +2,78 @@ package com.itacademy.jd2.is.carsharing.dao.orm.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+
+import org.hibernate.jpa.criteria.OrderImpl;
 import org.springframework.stereotype.Repository;
 
 import com.itacademy.jd2.is.carsharing.dao.api.IBrandDao;
 import com.itacademy.jd2.is.carsharing.dao.api.entity.IBrand;
 import com.itacademy.jd2.is.carsharing.dao.api.filter.BrandFilter;
 import com.itacademy.jd2.is.carsharing.dao.orm.impl.entity.Brand;
+import com.itacademy.jd2.is.carsharing.dao.orm.impl.entity.Brand_;
 
 @Repository
 public class BrandDaoImpl extends AbstractDaoImpl<IBrand, Integer> implements IBrandDao {
-	
+
 	protected BrandDaoImpl() {
 		super(Brand.class);
 	}
 
 	@Override
-	    public IBrand createEntity() {
-	        return new Brand();
-	    }
+	public IBrand createEntity() {
+		return new Brand();
+	}
 
 	@Override
-	public List<IBrand> find(BrandFilter filter)  {
-		throw new RuntimeException("Unimpliment");
+	public List<IBrand> find(BrandFilter filter) {
+		final EntityManager em = getEntityManager();
+		final CriteriaBuilder cb = em.getCriteriaBuilder();
+		final CriteriaQuery<IBrand> cq = cb.createQuery(IBrand.class); // define type of result
+		final Root<Brand> from = cq.from(Brand.class);// select from brand
+		cq.select(from); // select what? select *
+
+		if (filter.getSortColumn() != null) {
+			final SingularAttribute<? super Brand, ?> sortProperty = toMetamodelFormat(filter.getSortColumn());
+			final Path<?> expression = from.get(sortProperty); // build path to sort property
+			cq.orderBy(new OrderImpl(expression, filter.getSortOrder())); // order by column_name order
+		}
+
+		final TypedQuery<IBrand> q = em.createQuery(cq);
+		setPaging(filter, q);
+		return q.getResultList();
 	}
 
 	@Override
 	public long getCount(BrandFilter filter) {
-		throw new RuntimeException("Unimpliment");
+		final EntityManager em = getEntityManager();
+		final CriteriaBuilder cb = em.getCriteriaBuilder();
+		final CriteriaQuery<Long> cq = cb.createQuery(Long.class); // define type of result
+		final Root<Brand> from = cq.from(Brand.class); // select from brand
+		cq.select(cb.count(from)); // select what? select count(*)
+		final TypedQuery<Long> q = em.createQuery(cq);
+		return q.getSingleResult(); // execute query
+	}
+
+	private SingularAttribute<? super Brand, ?> toMetamodelFormat(final String sortColumn) {
+		switch (sortColumn) {
+		case "created":
+			return Brand_.created;
+		case "updated":
+			return Brand_.updated;
+		case "id":
+			return Brand_.id;
+		case "name":
+			return Brand_.name;
+		default:
+			throw new UnsupportedOperationException("sorting is not supported by column:" + sortColumn);
+		}
 	}
 
 }
